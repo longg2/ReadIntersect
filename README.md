cf4c0f30bf5b69258e0a7cab299a644df8221d83
longg2 <longg2@mcmaster.ca>
2022-03-11 12:08:50 -0500

    Updating to reflect changes prior to submission
    	• BlastLCA is now faster and no longer stops at the family level
    	• ReadIntersection.sh now has a variable BlastN composition
     	  threshold (i.e. X% of blast hits have to agree on a taxon)
    	• Will create a tmp folder to store intermediate files and
    	  delete it afterwards
    	• Creates a Logfile to show and store the settings

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Kraken2 and BlastN Ensemble Classification
-The script used to detect intersecting Blast and Kraken2 reads in "Examining pathogen DNA recovery across the remains of a 14th century Italian monk (St. Brancorsini) infected with _Brucella melitensis_" (Hider _et al._,In Review).
+The script used to detect intersecting Blast and Kraken2 reads in "Examining pathogen DNA recovery across the remains of a 14th century Italian friar (Giansante Brancorsini) infected with _Brucella melitensis_" (Hider _et al._,In Review).
 
 # Setup
-Place the three scripts in the same folder and run _ReadIntersection.sh_. This script assumes that the blast and Kraken runs have already been completed.
-
-## Blast Notes
-_BlastDefaultParser.awk_ is present to account for cases when the blastn output isn't tab deliminated. 
+Place the three scripts in the same folder and run _ReadIntersection.sh_. This script assumes that the Blast (default output) and Kraken runs have already been completed. Please note that this script requires the Standard Kraken Output.
 
 ## Requirements
-This script requires _taxonkit_ (https://github.com/shenwei356/taxonkit) and _R_ to run properly. The _R_ script will automatically download the required packages should they not be available.
+This script requires _taxonkit_ (https://github.com/shenwei356/taxonkit), _EUtils_ (https://dataguide.nlm.nih.gov/eutilities/how_eutilities_works.html) and _R_ to run properly. 
+The _R_ script will automatically download the required packages if they are not already present.
--- ReadIntersection.sh
+++ ReadIntersection.sh
@@ -1,73 +1,141 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 # These are the files and variables that will be needed
-usage() { printf 'Ensemble Classification Program V0.01
-	-b\tBlast Output File, default being used
-	-k\tKraken Output file.
-	-t\tNumber of CPU Threads to be used
-	-h\tShow this help message and exit\n' 1>&2; exit 1; }
+usage() { printf 'Ensemble Classification Program V0.02
 
-while getopts "b:k:t:h" arg; do
-	case $arg in
-		b)
-			blastTable=${OPTARG}
-			echo "${OPTARG} is your Blast Output File"
-			;;
-		k)
-			krakenOut=${OPTARG}
-			echo "${OPTARG} is your Kraken output file"
-			;;
-		t)
-			ncores=${OPTARG}
-			echo "${OPTARG} Number of CPU for Blast LCA Search"
-			;;
-		h | *)
-			usage
-			exit 0
-			;;
+	This pipline requires that taxonKit is installed and placed
+	in your PATH. <https://github.com/shenwei356/taxonkit>
+        -b\tBlast Output File
+	-k\tKraken Output (not the report!)
+	-o\tOutput Folder. Default = ReadIntersectionOut
+        -t\tNumber of CPU Threads for the BlastLCA analysis. Default = 8
+	-c\tMinimum Weight for BlastLCA [0,1]. Default = 0.75
+	-d\t(D)o not delete the tmp folder
+        -h\tShow this help message and exit\n' 1>&2; exit 1; }
 
-	esac
+log() {	printf "ReadIntersect settings for $(date):
+	Log File:\t${log}
+	Blast Input:\t${blastTable}
+	Kraken Input:\t${krakenOut}
+	Output folder:\t${out}
+	CPU Threads:\t${ncores}
+	-------------------------------------
+	Minimum Blast Composition:\t${compo}
+	-------------------------------------\n"; exit 0;
+}
+
+######################
+### Default values ###
+######################
+compo="0.75"
+ncores=8
+out="ReadIntersectionOut"
+scriptLocation=$(dirname $0)
+delete=1
+log="$(date +'%Y%m%d').log"
+
+##############
+### The UI ###
+##############
+while getopts "b:l:k:o:t:c:dh" arg; do
+        case $arg in
+                b)
+                        blastTable=${OPTARG}
+                        ;;
+                k)
+                        krakenOut=${OPTARG}
+                        ;;
+                o)
+                        out=${OPTARG}
+                        ;;
+                t)
+                        ncores=${OPTARG}
+                        ;;
+                c)
+                        compo=${OPTARG}
+                        ;;
+                d)
+                        delete=0
+                        ;;
+                l)
+                        log=${OPTARG}
+                        ;;
+                h | *)
+                        usage
+                        exit 0
+                        ;;
+
+        esac
 done
-exit 0
+log | tee $log # The inital log file
 
+mkdir -p $out # Since we'll be making subfolders, we'll make this first
+mkdir -p $out/tmp # Intermediate files located here
+mkdir -p $out/Blast
+
+sample=$(basename $blastTable)
+sample=${sample%.*}
 ################
 # Blast Output #
 ################
-echo "Let's start with Blast"
+echo "Analyzing Blast Results"
 # First let's take care of the blast file.  This'll take the longest time...
-# NOTE: Assuming no taxa ids are present
 echo "Pulling out the Accessions"
-./BlastDefaultParser.awk $blastTable > ModBlast.table # If the default blast setting was used
-#cut -f 1,2 $blastTable | sed -e "s/\.[0-9]$//"> ModBlast.table # Only useful if tab 6 was used
-cut -f 2 ModBlast.table | sort | uniq > accessions.list
-#
+$scriptLocation/BlastDefaultParser.awk $blastTable > $out/tmp/ModBlast.table # If the default blast setting was used
+cut -f 2 $out/tmp/ModBlast.table | sort -u --compress-program gzip > $out/tmp/accessions.list
+
 ## Now we need to get their respective Taxa IDs. Code from https://www.biostars.org/p/353500/
 echo "Translating the Accessions to TaxaIDs"
-cat accessions.list | epost -db nuccore | esummary -db nuccore |\
-      	xtract -pattern DocumentSummary -element Caption,TaxId > AccessionTaxaIds.table
-
+cat $out/tmp/accessions.list | epost -db nuccore | esummary|\
+        xtract -pattern DocumentSummary -element Caption,TaxId > $out/tmp/AccessionTaxaIds.table
+ 
 # Merging the files together prior to determining the lineages
-sed -i -e "s/\.[0-9]$//g" ModBlast.table # Removes the version numbers from accession IDs
-join -1 2 -2 1 <(sort -k2 ModBlast.table) <(sort -k1 AccessionTaxaIds.table) | cut -f 2,3 -d " " > SeqWithTaxa.table
+sed -i -e "s/\.[0-9]$//g" $out/tmp/ModBlast.table # Removes the version numbers from accession IDs
+
+#### Sorting issues.  Can't do it in a single line...
+sort -k2 --compress-program gzip $out/tmp/ModBlast.table > $out/tmp/ModBlastSorted.table
+sort -k1 --compress-program gzip $out/tmp/AccessionTaxaIds.table > $out/tmp/AccessionTaxaIdsSorted.table
+
+join -1 2 -2 1 $out/tmp/ModBlastSorted.table $out/tmp/AccessionTaxaIdsSorted.table | cut -f 2,3 -d " " | sort | uniq -c > $out/tmp/SeqWithTaxa.table
+sed -i -e "s/^ *//g" -e "s/ /\t/g" $out/tmp/SeqWithTaxa.table
 
 # Determining the Lineages.  Using taxonkit
-sort SeqWithTaxa.table -k 3 > SeqWithTaxaSorted.table
-cut -f 3 SeqWithTaxaSorted.table | uniq | taxonkit lineage |\ 
-	taxonkit reformat -t | cut -f 1,4 | sed "s/;/\t/g" > taxonKitBlast.out
+echo "Getting the taxonomic lineages using taxonKit"
+sort $out/tmp/SeqWithTaxa.table -k 3 > $out/tmp/SeqWithTaxaSorted.table
+cut -f 3 $out/tmp/SeqWithTaxaSorted.table | uniq | taxonkit lineage |\
+        taxonkit reformat -t | cut -f 1,4 | sed "s/;/\t/g" > $out/tmp/taxonKitBlast.out
 
-join -1 3 -2 1 SeqWithTaxaSorted.table taxonKitBlast.out | cut -f 2- -d " " |\
-       	sed -e "s/ /\t/g" | sort -k 2 > LineagedBlastData.delim
+join -1 3 -2 1 $out/tmp/SeqWithTaxaSorted.table $out/tmp/taxonKitBlast.out | cut -f 2- -d " " |\
+        sed -e "s/ /\t/g" | sort -k 2 > $out/tmp/LineagedBlastData.delim
 
 # After this, we need to figure out the LCAs for each Blast Hit.
-BlastLCA.R LineagedBlastData.delim $ncores BlastLCAout.tab
+./$scriptLocation/BlastLCA.R $out/tmp/LineagedBlastData.delim $ncores $out/Blast/${sample}Blast.tab $compo
 
-#################
-# Kraken Output #
-#################
+######################
+### Kraken Output ####
+######################
+mkdir -p $out/Kraken
+
 echo "Now we'll look at Kraken"
+grep -v "^U" $krakenOut | cut -f 2,3 | sort -k 2 > $out/tmp/Krak.tmp # Sorting the Kraken file based on the taxa and removing unclassified reads from the analysis
 
-grep -v "0$" $krakenOut | sort -k 3 > Krak.tmp # Sorting the Kraken file based on the taxa and removing unclassified reads from the analysis
+cut -f 2 $out/tmp/Krak.tmp | uniq | taxonkit lineage |\
+	taxonkit reformat -t | cut -f 1,4 | sed "s/;/\t/g" > $out/tmp/taxonKitKraken.out
 
-cut -f 3 Krak.tmp | uniq | taxonkit lineage |\
-	taxonkit reformat -t | cut -f 1,4 | sed "s/;/\t/g" > taxonKitKraken.out
+join -1 2 -2 1 $out/tmp/Krak.tmp $out/tmp/taxonKitKraken.out | cut -f 2- -d " " | sed -e "s/ /\t/g" > $out/tmp/tmpKrakenOut.tab
+head -n 1 $out/Blast/${sample}Blast.tab > $out/tmp/tmpHeader.tmp
+cat $out/tmp/tmpHeader.tmp $out/tmp/tmpKrakenOut.tab > $out/Kraken/${sample}Kraken.tab 
 
-join -1 3 -2 1 Krak.tmp taxonKitKraken.out | cut -f 3- -d " " | sed -e "s/ /\t/g" > KrakenTable.out
+#######################################
+#### Now to get the final Results. ####
+#######################################
+mkdir -p $out/Intersections
+
+./$scriptLocation/FinalStep.R $out/Blast/${sample}Blast.tab $out/Kraken/${sample}Kraken.tab $out/Intersections/$sample
+
+if [[ $delete == 1 ]]
+then
+	rm -rf $out/tmp/
+fi
+
+echo "ReadIntersect analysis Successful for $sample"
+exit 0
